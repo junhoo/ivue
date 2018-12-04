397733ee855d3ffee435da9a6ed31c3cab77f18f
junhoo <547455277@qq.com>
2018-12-04 11:43:02 +0800

    阻止冒泡

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click="xxx">
-    <div class="content-wrapper" v-if="visible">
+  <div class="popover" @click.stop="xxx">
+    <div class="content-wrapper" v-if="visible" @click.stop>
       <slot name="content"></slot>
     </div>
     <slot></slot>
@@ -15,6 +15,19 @@
     methods: {
       xxx () {
         this.visible = !this.visible
+        if (this.visible) {
+          this.$nextTick(() => {
+            let eventHandler = () => {
+              this.visible = false
+              console.log('document 隐藏 popover')
+              document.removeEventListener('click', eventHandler)
+            }
+
+            document.addEventListener('click', eventHandler)
+          })
+        } else {
+          console.log('v-model 隐藏 popover')
+        }
       }
     }
   }
