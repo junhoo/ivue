128fb73c80893c09f3b212e6d114b61d0782fe08
junhoo <547455277@qq.com>
2018-12-04 15:07:39 +0800

    overflow:hidden浮动元素bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click.stop="onClick">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
  <script>
@@ -13,10 +15,14 @@
       return {visible: false}
     },
     methods: {
-      xxx () {
+      onClick () {
         this.visible = !this.visible
         if (this.visible) {
           this.$nextTick(() => {
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+            this.$refs.contentWrapper.style.top = top + window.scrollX + 'px'
             let eventHandler = () => {
               this.visible = false
               console.log('document 隐藏 popover')
@@ -37,12 +43,11 @@
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper {
-      position: absolute;
-      bottom: 100%;
-      left: 0;
-      border: 1px solid red;
-      box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    }
+  }
+  .content-wrapper {
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    transform: translateY(-100%);
   }
 </style> 
