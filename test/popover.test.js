0b3e88fb1a663b18308377ef4129154807a04b26
junhoo <547455277@qq.com>
2018-12-13 14:27:34 +0800

    commit hover use case

--- test/popover.test.js
+++ test/popover.test.js
@@ -1,12 +1,16 @@
 const expect = chai.expect;
 import Vue from 'vue'
 import Popover from '../src/popover'
+
 Vue.config.productionTip = false
 Vue.config.devtools = false
+
 describe('Popover', () => {
+  
   it('存在.', () => {
     expect(Popover).to.exist
   })
+
   it('可以设置position.', (done) => {
     Vue.component('g-popover', Popover)
     const div = document.createElement('div')
@@ -28,5 +32,30 @@ describe('Popover', () => {
       expect(contentWrapper.classList.contains('position-bottom')).to.be.true
       done()
     })
+  })
+  xit('可以设置 trigger', (done) => {
+    Vue.component('g-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+    <g-popover trigger="hover" ref="a">
+      <template slot="content">
+      弹出内容
+      </template>
+      <button>点击</button>
+    </g-popover>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    setTimeout(() => {
+      let event = new Event('mouseenter');
+      vm.$el.dispatchEvent(event)
+      vm.$nextTick(() => {
+        const { contentWrapper } = vm.$refs.a.$refs
+        expect(contentWrapper).to.exist
+        done()
+      })
+    }, 200)
   })
 })
