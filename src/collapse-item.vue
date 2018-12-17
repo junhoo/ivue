389bfb2824b87b3ae8453e0578a4e9fe34748f1d
junhoo <547455277@qq.com>
2018-12-17 15:36:27 +0800

    提供数据

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="collapseItem">
-    <div class="title" @click="open=!open">
+    <div class="title" @click="toggle">
       {{title}}
     </div>
     <div class="content" v-if="open">
@@ -22,6 +22,27 @@
       return {
         open: false
       }
+    },
+    inject: ['eventBus'],
+    mounted () {
+      this.eventBus && this.eventBus.$on('update:selected', (vm) => {
+        if (vm !== this) {
+          this.close()
+        }
+      })
+    },
+    methods: {
+      toggle () {
+        if (this.open) {
+          this.open = false
+        } else {
+          this.open = true
+          this.eventBus && this.eventBus.$emit('update:selected', this)
+        }
+      },
+      close () {
+        this.open = false
+      }
     }
   }
 </script>
@@ -35,6 +56,7 @@
       margin-top: -1px;
       margin-left: -1px;
       margin-right: -1px;
+      border: 1px solid $grey;
       min-height: 32px;
       display: flex;
       align-items: center;
